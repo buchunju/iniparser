1b60febfee0620634b25206f44f6422ab8e18fc4
buchunju <buchunjukenneth@gmail.com>
2025-04-10 16:38:10 +0300

    Updated test

--- test.cpp
+++ test.cpp
@@ -6,40 +6,93 @@
 ** Email   : buchunjukenneth@gmail.com
 **
 ** MODIFIED : [22-11-23] rewrite
+** MODIFIED : [10-04-25] updated test
 **
 ** Copyright 2020 Kenneth Buchunju <buchunjukenneth@gmail.com>
 **
 ******************************************************/
 
 #include <iostream>
-
+#include <cassert>
 #include "ini.h"
 
-int main(int argc, char *argv[])
+void testSaveAndParse()
+{
+    IniParser parser("test.ini");
+
+    // Save to file
+    parser.addSection("Books");
+    parser.addValue("title", "Harry Potter");
+    parser.addValue("year", 2017);
+    parser.addValue("price", 19.99);
+    parser.addValue("available", true);
+    parser.save();
+
+    // Parse from file
+    parser.parse("test.ini");
+
+    // Assertions
+    assert(parser["Books"].getString("title") == "Harry Potter");
+    assert(parser["Books"].getInt("year") == 2017);
+    assert(parser["Books"].getFloat("price") == 19.99f);
+    assert(parser["Books"].getBoolean("available") == true);
+
+    std::cout << "testSaveAndParse passed!" << std::endl;
+}
+
+void testMissingSection()
+{
+    IniParser parser("test.ini");
+
+    try
+    {
+        parser["NonExistent"].getString("key");
+        assert(false); // Should not reach here
+    }
+    catch (const IniError &e)
+    {
+        assert(e.toString().find("Section 'NonExistent' not found") != std::string::npos);
+    }
+
+    std::cout << "testMissingSection passed!" << std::endl;
+}
+
+void testMissingKey()
+{
+    IniParser parser("test.ini");
+    parser.addSection("Movies");
+    parser.addValue("title", "Inception");
+    parser.save();
+
+    parser.parse("test.ini");
+
+    try
+    {
+        parser["Movies"].getString("director");
+        assert(false); // Should not reach here
+    }
+    catch (const IniError &e)
+    {
+        assert(e.toString().find("not in Movies") != std::string::npos);
+    }
+
+    std::cout << "testMissingKey passed!" << std::endl;
+}
+
+int main()
 {
     try
     {
-        IniParser parser("test.ini");
-
-        //
-        // example save to file
-        parser.addSection("Books");
-        parser.addValue("title", "Harry Potter");
-        parser.addValue("year", 2017);
-        parser.save();
-
-        //
-        // example parse from file
-        parser.parse("test.ini");
-
-        cout << "Section: Books" << endl;
-        cout << "title:  " << parser["Books"].getString("title") << endl;
-        cout << "year: " << parser["Books"].getInt("year") << endl;
+        testSaveAndParse();
+        testMissingSection();
+        testMissingKey();
     }
-    catch (IniError e)
+    catch (const IniError &e)
     {
-        cout << e.toString() << endl;
+        std::cerr << "Test failed: " << e.toString() << std::endl;
+        return 1;
     }
 
+    std::cout << "All tests passed!" << std::endl;
     return 0;
 }
